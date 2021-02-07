c3637e766385a98a8f621a49a34d67380790aac4
Stan Janssen <mail@janssen.io>
2021-02-07 12:33:39 +0100

    Added ASN plugin

---
+++ TransactionQL.Plugins.ASN/ASN.fs
@@ -0,0 +1,50 @@
+﻿namespace TransactionQL.Plugins
+
+open System
+open System.IO
+open System.Globalization
+open FSharp.Data
+open TransactionQL.Parser.AST
+open TransactionQL.Parser.QLInterpreter
+open TransactionQL.Input.Converters
+
+module ING =
+
+    let private dateFormat = "dd/MM/yyyy"
+
+    type AsnTransactions = CsvProvider<"ASN.csv">
+    type IngReader () =
+        interface IConverter with
+            member this.DateFormat = dateFormat;
+            member this.Read (FilePath fname) =
+                let trxs = AsnTransactions.Load((new StreamReader(fname)))
+                trxs.Rows
+                |> Seq.map (fun row ->
+                    let amount = Decimal.Parse(row.Transactiebedrag, NumberStyles.AllowLeadingSign ||| NumberStyles.AllowDecimalPoint)
+                    let isSent = amount < 0m
+                    Map.ofList [
+                        ("Sender",      if isSent then row.Opdrachtgeversrekening else row.Tegenrekeningsnummer)
+                        ("Receiver",    if isSent then row.Tegenrekeningsnummer else row.Opdrachtgeversrekening)
+                        ("Amount",      string amount)
+                        ("Total",       (string <| Math.Abs amount))
+                        ("Date",        row.Boekingsdatum)
+                        ("Description", row.Omschrijving)
+                        ("Name",        row.``Naam Tegenrekening``)
+                    ]
+                )
+
+            member this.Map row =
+                let fromRow col = Map.find col row
+                {
+                    Header = 
+                        Header (
+                            DateTime.ParseExact(fromRow "Date", dateFormat, CultureInfo.InvariantCulture),
+                            fromRow "Name"
+                        )
+                    Lines = [
+                        Line (Account [fromRow "Sender"], Some (Commodity "EUR", float (fromRow "Amount")))      
+                        Line (Account [fromRow "Receiver"], None)
+                    ]
+                    Comments = 
+                        [ fromRow "Description" ]
+                }
--- publish.sh
+++ publish.sh
@@ -1,3 +1,4 @@
-dotnet publish TransactionQL.Console/TransactionQL.Console.fsproj -r linux-x64 -p:PublishSingleFile=true --self-contained=false
+dotnet publish TransactionQL.Console/TransactionQL.Console.fsproj -r linux-x64 -p:PublishSingleFile=true
 dotnet publish TransactionQL.Plugins.ING/TransactionQL.Plugins.ING.fsproj -r linux-x64
 dotnet publish TransactionQL.Plugins.Bunq/TransactionQL.Plugins.Bunq.fsproj -r linux-x64
+dotnet publish TransactionQL.Plugins.ASN/TransactionQL.Plugins.ASN.fsproj -r linux-x64
