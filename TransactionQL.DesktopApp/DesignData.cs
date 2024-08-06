008ef51bf4d378b0075845028f829a94016e10d3
Stan Ionitoiu-Janssen <mail@janssen.io>
2024-08-06 18:08:29 +0200

    package upgrades and further styling

--- TransactionQL.DesktopApp/DesignData.cs
+++ TransactionQL.DesktopApp/DesignData.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TransactionQL.DesktopApp.ViewModels;
 
 namespace TransactionQL.DesktopApp;
@@ -12,26 +7,41 @@ public static class DesignData
 {
     public static readonly PaymentDetailsViewModel PaymentDetails = new(
         "Green Energy",
-        new DateTime(2023, 03, 14),
+        new DateTime(2023, 03, 14, 0, 0, 0, DateTimeKind.Utc),
         "Payment Note 32458 Electricity Bill 03206138 01-02-2023",
         "€",
         -133.70m,
-        new ObservableCollection<string> { "Test", "Test2", "Test3" }
+        ["Test", "Test2", "Test3"]
     )
     {
-        Postings = new ObservableCollection<Posting>
-        {
+        Postings =
+        [
             new() { Account = "Assets:Checking", Currency = "EUR", Amount = -127.11m },
             new() { Account = "Expenses:Living:Utilities" }
-        },
+        ],
         IsActive = true
+    };
+
+    public static readonly PaymentDetailsViewModel PaymentDetails2 = new(
+        "Test",
+        new DateTime(2023, 03, 14, 0, 0, 0, DateTimeKind.Utc),
+        "Payment Note 32458 Electricity Bill 03206138 01-02-2023",
+        "€",
+        -133.70m,
+        ["Test", "Test2", "Test3"]
+    )
+    {
+        HasError = true,
+        Postings =
+        [
+            new() { Account = "Assets:Checking", Currency = "EUR", Amount = -127.11m },
+            new() { Account = "Expenses:Living:Utilities" }
+        ],
+        IsActive = false
     };
 
     public static readonly MainWindowViewModel MainWindow = new()
     {
-        BankTransactions = new ObservableCollection<PaymentDetailsViewModel>
-        {
-            PaymentDetails, PaymentDetails
-        }
+        BankTransactions = [PaymentDetails, PaymentDetails2 ]
     };
 }
