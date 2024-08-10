ee4193f4900d3d18ca1d9be0255ec5c5bd102280
Stan Ionitoiu-Janssen <mail@janssen.io>
2024-08-11 00:01:15 +0200

    Custom draggable mini-window

--- TransactionQL.DesktopApp/Views/DataWizardWindow.axaml.cs
+++ TransactionQL.DesktopApp/Views/DataWizardWindow.axaml.cs
@@ -45,4 +45,34 @@ public partial class DataWizardWindow : Window
             this.Close();
         }
     }
+
+    private void Close(object? sender, RoutedEventArgs e)
+    {
+        this.Close();
+    }
+
+    private bool _mouseDownForWindowMoving = false;
+    private PointerPoint _originalPoint;
+
+    private void Dragbar_OnPointerMoved(object? sender, PointerEventArgs e)
+    {
+        if (!_mouseDownForWindowMoving) return;
+
+        PointerPoint currentPoint = e.GetCurrentPoint(this);
+        Position = new PixelPoint(Position.X + (int)(currentPoint.Position.X - _originalPoint.Position.X),
+            Position.Y + (int)(currentPoint.Position.Y - _originalPoint.Position.Y));
+    }
+
+    private void Dragbar_OnPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (WindowState == WindowState.Maximized || WindowState == WindowState.FullScreen) return;
+
+        _mouseDownForWindowMoving = true;
+        _originalPoint = e.GetCurrentPoint(this);
+    }
+
+    private void Dragbar_OnPointerReleased(object? sender, PointerReleasedEventArgs e)
+    {
+        _mouseDownForWindowMoving = false;
+    }
 }
